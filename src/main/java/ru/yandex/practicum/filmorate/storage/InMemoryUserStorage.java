b1d9a90288eeb3e1ddce8e09a58b2d55230d414b
IvanMakarov3301 <ivanmakarovxy@gmail.com>
2026-02-18 22:55:14 +0300

    Вынести хранилища в отдельные классы

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -0,0 +1,44 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.*;
+
+@Component
+public class InMemoryUserStorage implements UserStorage {
+
+    private final Map<Integer, User> users = new HashMap<>();
+    private int nextId = 1;
+
+    @Override
+    public User createUser(User user) {
+        user.setId(nextId++);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User getUser(int id) {
+        User user = users.get(id);
+        if (user == null) {
+            throw new NotFoundException("User not found");
+        }
+        return user;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public User updateUser(User user) {
+        if (!users.containsKey(user.getId())) {
+            throw new NotFoundException("User not found");
+        }
+        users.put(user.getId(), user);
+        return user;
+    }
+}
