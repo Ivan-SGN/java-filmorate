27f83b8094c326444fe31b4b51ad764b58b38e3d
IvanMakarov3301 <ivanmakarovxy@gmail.com>
2026-03-12 23:39:24 +0300

    Добавить реализацию лайков для  InMemoryFilmStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -5,7 +5,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.*;
 
-@Component
 public class InMemoryFilmStorage implements FilmStorage {
 
     private final Map<Integer, Film> films = new HashMap<>();
@@ -19,16 +18,6 @@ public class InMemoryFilmStorage implements FilmStorage {
     }
 
     @Override
-    public Optional<Film> getFilm(int id) {
-        return Optional.ofNullable(films.get(id));
-    }
-
-    @Override
-    public List<Film> getAllFilms() {
-        return new ArrayList<>(films.values());
-    }
-
-    @Override
     public Optional<Film> updateFilm(Film film) {
         if (!films.containsKey(film.getId())) {
             return Optional.empty();
@@ -38,15 +27,38 @@ public class InMemoryFilmStorage implements FilmStorage {
     }
 
     @Override
-    public void addLike(int filmId, int userId) {
+    public Optional<Film> getFilm(int id) {
+        return Optional.ofNullable(films.get(id));
     }
 
     @Override
-    public void removeLike(int filmId, int userId) {
+    public List<Film> getAllFilms() {
+        return films.values().stream().toList();
     }
 
     @Override
     public List<Film> getPopularFilms(int count) {
-        return List.of();
+        List<Film> result = new ArrayList<>(films.values());
+        result.sort((f1, f2) -> Integer.compare(f2.getLikes().size(), f1.getLikes().size()));
+        if (result.size() > count) {
+            return result.subList(0, count);
+        }
+        return result;
+    }
+
+    @Override
+    public void addLike(int filmId, int userId) {
+        Film film = films.get(filmId);
+        if (film != null) {
+            film.getLikes().add(userId);
+        }
+    }
+
+    @Override
+    public void removeLike(int filmId, int userId) {
+        Film film = films.get(filmId);
+        if (film != null) {
+            film.getLikes().remove(userId);
+        }
     }
 }
