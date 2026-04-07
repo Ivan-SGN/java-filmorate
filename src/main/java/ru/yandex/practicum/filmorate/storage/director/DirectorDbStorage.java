647f98a859ffab177ba5060e4411caf611c66b07
Nikita <nikita220898@gmail.com>
2026-04-08 01:23:30 +0300

    feat: add film search by title and director

--- src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorage.java
@@ -4,6 +4,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.storage.BaseRepository;
 
@@ -61,6 +62,10 @@ public class DirectorDbStorage extends BaseRepository<Director> implements Direc
 
     @Override
     public Director update(Director director) {
+        Optional<Director> existing = getById(director.getId());
+        if (existing.isEmpty()) {
+            throw new NotFoundException("Director not found");
+        }
         update(UPDATE, director.getName(), director.getId());
         return director;
     }
